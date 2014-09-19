2f24b9614728a6d3f53c54e493babe01d4cc1046
jiangxiaoming <jiangxiaomjing@conew.com>
2014-09-19 19:01:24 +0800

    [Update]

--- src/com/test/gl_draw/test/GLZoomView.java
+++ src/com/test/gl_draw/test/GLZoomView.java
@@ -7,7 +7,12 @@ import com.test.gl_draw.glview.GLTextureView;
 public class GLZoomView extends GLTextureView {
 	private RectF mStartRect = new RectF();
 	private RectF mEndRect = new RectF();
-	
+
+	@Override
+	public boolean onDown(float x, float y) {
+		return true;
+	}
+
 	public void SetZoomRect(RectF start, RectF end) {
 		mStartRect.set(start);
 		mEndRect.set(end);
